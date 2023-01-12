4bb015bed138c1fdb11566ebc06df9b1a531df06
delanobmarques <delanobmarques@gmail.com>
2023-01-12 15:51:31 -0400

    install react-date-range and date-fns to be used in search, improve header component

--- src/pages/list/List.page.jsx
+++ src/pages/list/List.page.jsx
@@ -1,6 +1,12 @@
+import Header from "../../components/header/Header.component"
+import Navbar from "../../components/navbar/Navbar.component"
+
 const List = () => {
   return (
-    <div>List</div>
+    <>
+        <Navbar/>
+        <Header type="list"/>
+    </>
   )
 }
 
