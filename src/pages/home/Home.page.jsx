f125b6b00a30cf03c555012436cd83c42c4bb2fa
delanobmarques <delanobmarques@gmail.com>
2023-01-12 10:10:34 -0400

    create and style navbar component basic structure

--- src/pages/home/Home.page.jsx
+++ src/pages/home/Home.page.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import Navbar from "../../components/navbar/Navbar.component"
+import "./home.styles.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+        <Navbar />       
+    </>
   )
 }
 
